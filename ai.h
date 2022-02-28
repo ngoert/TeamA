063b40ccca5b8cbc25dda1bb56ed648051cda9db
rhallock <hallockr@sbcglobal.net>
2022-02-27 21:36:24 -0600

    Update ai.h

--- ai.h
+++ ai.h
@@ -2,7 +2,4 @@
 #define AI_H
 char** getWords(char* input, int* n);
 void swap(char* a, char* b);
-char* removeWord(int i, char** words, int* n);
-void checkWords(char* word, char** words, int* n);
-char* getWord(char** words, int* n);
 #endif
