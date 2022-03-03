b5c500ae51f11a97f915608bd497e59747908efa
Cooper Tush <61853816+Coopa-T@users.noreply.github.com>
2022-03-02 21:37:19 -0600

    Final Updated serverFunc

--- serverFunc.c
+++ serverFunc.c
@@ -1,3 +1,8 @@
+//Cooper Tush
+//Group A
+//cooper.tush@okstate.edu
+//March 2nd, 2022
+
 #include <stdio.h>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -12,26 +17,27 @@
 #define PORT 8081
 #define SA struct sockaddr
 
+/*
+After the server connects to the client the server sends them this menu that ask if they want to play in single-player, multiplayer, or to just quit the game. Also saves all the player's information just in case.
+*/
+
 void func(int connf)
 {
-	char firstName[40];
-	char lastName[40];
-	char country[40];
-	char playerInfo[120];
-    char buffer[MAX];
-    int a;
+	int a;
+	char buffer[MAX];	//Size of buffer
+	char firstName[40];	//Size of First Name
+	char lastName[40];	//Size of Last Name
+	char country[40];	//Size of Country
+
     // infinite loop for chat
     for (;;) {
         bzero(buffer, MAX);
-   
-        // read the message from client and copy it in buffer
-        // read(connf, buffer, sizeof(buffer));
+        
         //print buffer which contains the client contents
-        //printf("Message from client: %s\t Message to client : ", buffer);
         bzero(buffer, MAX);
         a = 0;
+        
         // copy server message in the buffer
-        // while ((buffer[a++] = getchar()) != '\n');
         strcpy(buffer, "Welcome to main!" 
            "\n1. Single-Player Mode"
            "\n2. Multi-Player Mode"
@@ -40,47 +46,49 @@ void func(int connf)
    
         // and send that buffer to client
         write(connf, buffer, strlen(buffer)+1);
+        // read the message from client and copy it in buffer
         read(connf, buffer, strlen(buffer)+1);
 
-		
+	//Starts Single-Player
         if(buffer[0] == '1'){
 			strcpy(buffer, "Single-Player Mode\n"
 			"Player info enter first name: ");
+			
+			//Takes First Name
 			write(connf, buffer, sizeof(buffer));
 			read(connf, buffer, strlen(buffer)+1);
 			strcpy(firstName, buffer);
-			//strcat(playerInfo, "First Name: ");
-			//strcat(playerInfo, firstName);
+			
+			//Take Last Name
 			strcpy(buffer, "Enter last name: ");
 			write(connf, buffer, sizeof(buffer));
 			read(connf, buffer, strlen(buffer)+1);
 			strcpy(lastName, buffer);
-			//strcat(playerInfo, "Last Name: ");
-			//strcat(playerInfo, lastName);
+			
+			//Takes Country
 			strcpy(buffer, "Enter country: ");
 			write(connf, buffer, sizeof(buffer));
 			read(connf, buffer, strlen(buffer)+1);
 			strcpy(country, buffer);
-			//strcat(playerInfo, "Country: ");
-			//strcat(playerInfo, country);
-		 
-			//strcat(playerInfo, "\n");
-			//strcpy(buffer, playerInfo);
 		
 			firstName[strlen(firstName)-1] = '\0';
 			lastName[strlen(lastName)-1] = '\0';
 			country[strlen(country)-1] = '\0';
 		
 			printf("End of player info\n");
-		
+			
+			//Starts Single-Player Mode
 			playGame(connf, firstName, lastName, country);
         }
+        //Multiplayer when client inputs 2
         else if (buffer[0] == '2'){
         	strcpy(buffer, "Multi-Player Mode");
         	write(connf, buffer, sizeof(buffer));
+            //Goes into Multiplayer Mode
             //multiPlayerMode();
         }
         
+        //Quits when client inputs 3
         else if (buffer[0] == '3'){
         	strcpy(buffer, "Server is now exiting...\n"
         			"Goodbye\n");
--- serverFunc.h
+++ serverFunc.h
@@ -1,3 +1,4 @@
+//Header file for serverFunc.c
 #ifndef SERVERFUNC_H
 #define SERVERFUNC_H
 void func(int connf);
